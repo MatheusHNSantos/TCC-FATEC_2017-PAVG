93c8db7eb90084a584745336f59e4c4a0a08600f
felipemantoan <luisfelipemantoan@gmail.com>
2017-07-10 20:20:52 -0300

    Acrescimo da classe de conexao simples

--- src/dao/ConnectionFactory.java
+++ src/dao/ConnectionFactory.java
@@ -1,14 +1,46 @@
 package dao;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.JDBCType;
 import java.sql.SQLException;
+
 /**
  *
  * @author felipemantoan
  */
-public abstract class ConnectionFactory implements Connection{
+public abstract class ConnectionFactory{
+    
+    private Connection conn;
+    private String driver;
+    private String login;
+    private String senha;
+    private String banco;
+    private String host;
+    private String porta;
+    private final String dns;
     
     ConnectionFactory() {
+        
+        this.driver = "mysql";
+        this.login = "root";
+        this.senha = "root";
+        this.banco = "BancoTeste";
+        this.host = "localhost";
+        this.porta = "3306";
+        this.dns = "jdbc:" + this.driver + "://" + this.host + ":" + this.porta +"/" + banco;
+    }
+    
+    public void debug(){
+        System.out.println(this.dns);
+    }
+    
+    public Connection getConnection() throws SQLException, ClassNotFoundException{
+        if (this.conn == null) {
+            this.conn = DriverManager.getConnection(this.dns, this.login, this.senha);
+        }
+        else {
+            return this.conn;
+        }
+        
+        return null;
     }
 }
