37ea6c7077195a6ea5e73fbac3d40bfacb89e22b
felipemantoan <luisfelipemantoan@gmail.com>
2017-07-12 18:56:18 -0300

    Acrescimo de classe usuário dao

---
+++ src/dao/UsuarioDAO.java
@@ -0,0 +1,42 @@
+package dao;
+
+import model.Entity;
+import java.util.ArrayList;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author felipemantoan
+ */
+public class UsuarioDAO extends CRUD{
+
+    @Override
+    public boolean create() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean read(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean update(Entity entity, int id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean delete() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ArrayList readAll() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
