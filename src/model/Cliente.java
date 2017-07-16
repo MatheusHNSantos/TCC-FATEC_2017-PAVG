58c0e015ecd0b3570751241d6189077226f534b5
felipemantoan <luisfelipemantoan@gmail.com>
2017-07-16 13:28:19 -0300

    Criação do atributos

--- src/model/Cliente.java
+++ src/model/Cliente.java
@@ -11,4 +11,30 @@ package model;
  */
 public class Cliente extends Pessoa{
     
+    private String CPF;
+    private String RG;
+
+    public String getCPF() {
+        return CPF;
+    }
+
+    public void setCPF(String CPF) {
+        this.CPF = CPF;
+    }
+
+    public String getRG() {
+        return RG;
+    }
+
+    public void setRG(String RG) {
+        this.RG = RG;
+    }
+    
+    public boolean validarCPF() {
+        return false;
+    }
+    
+    public boolean validarRG() {
+        return false;
+    }
 }
