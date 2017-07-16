73410733272ec7255c3cf0db4cfac6bb9d0b21e2
felipemantoan <luisfelipemantoan@gmail.com>
2017-07-16 13:15:51 -0300

    Acrescimo da classe abstrata pessoa

---
+++ src/model/Pessoa.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author felipemantoan
+ */
+public abstract class Pessoa {
+    
+    protected int id;
+    protected String nome;
+    protected String telefone;
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+}
