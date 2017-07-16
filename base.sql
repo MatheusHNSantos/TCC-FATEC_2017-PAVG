5fb75af0ee91afb23e448159951a76633716faa9
felipemantoan <luisfelipemantoan@gmail.com>
2017-07-16 12:45:41 -0300

    Nova tabela de pessoa fisica

--- base.sql
+++ base.sql
@@ -7,3 +7,12 @@ CREATE TABLE IF NOT EXISTS pessoa(
     telefone_pessoa varchar(11) not null,
     CONSTRAINT PRK_ID_PESSOA PRIMARY KEY (id_pessoa) 
 );
+
+CREATE TABLE IF NOT EXISTS pessoa_fisica(
+	rg varchar(8) not null,
+    cpf varchar(11) not null,
+    id_pessoa int not null,
+    CONSTRAINT UNK_CPF_PESSOA_FISICA UNIQUE KEY (cpf),
+    CONSTRAINT UNK_RG_PESSOA_FISICA UNIQUE KEY (rg),
+    CONSTRAINT FRK_ID_PESSOA_FISICA FOREIGN KEY (id_pessoa) REFERENCES pessoa(id_pessoa)
+);
