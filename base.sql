c7d48cd213d48679841e77c3620708ae80988a12
felipemantoan <luisfelipemantoan@gmail.com>
2017-08-13 17:53:59 -0300

    Alteração no padrão de SQL

--- base.sql
+++ base.sql
@@ -1,43 +1,43 @@
 CREATE DATABASE IF NOT EXISTS banco_testes;
 USE banco_testes;
 
-CREATE TABLE IF NOT EXISTS pessoa(
-	id_pessoa int not null auto_increment,
-    nome_pessoa varchar(255) not null,
-    telefone_pessoa varchar(11) not null,
-    CONSTRAINT PRK_ID_PESSOA PRIMARY KEY (id_pessoa) 
+CREATE TABLE IF NOT EXISTS people(
+	id_people int not null auto_increment,
+    name_people varchar(255) not null,
+    phone_people varchar(11) not null,
+    CONSTRAINT PRK_ID_PEOPLE PRIMARY KEY (id_people) 
 );
 
-CREATE TABLE IF NOT EXISTS pessoa_fisica(
+CREATE TABLE IF NOT EXISTS costumer(
 	rg varchar(10) not null,
     cpf varchar(11) not null,
-    id_pessoa int not null,
-    CONSTRAINT UNK_CPF_PESSOA_FISICA UNIQUE KEY (cpf),
-    CONSTRAINT UNK_RG_PESSOA_FISICA UNIQUE KEY (rg),
-    CONSTRAINT FRK_ID_PESSOA_FISICA FOREIGN KEY (id_pessoa) REFERENCES pessoa(id_pessoa)
+    id_people int not null,
+    CONSTRAINT UNK_CPF_COSTUMER UNIQUE KEY (cpf),
+    CONSTRAINT UNK_RG_COSTUMER UNIQUE KEY (rg),
+    CONSTRAINT FRK_ID_COSTUMER FOREIGN KEY (id_people) REFERENCES people(id_people)
 );
 
-CREATE TABLE IF NOT EXISTS pessoa_juridica(
+CREATE TABLE IF NOT EXISTS supplier(
 	cnpj varchar(14) not null,
-    id_pessoa int not null,
-    CONSTRAINT UNK_CNPJ_PESSOA_JURIDICA UNIQUE KEY (cnpj),
-    CONSTRAINT FRK_ID_PESSOA_JURIDICA FOREIGN KEY (id_pessoa) REFERENCES pessoa(id_pessoa)
+    id_people int not null,
+    CONSTRAINT UNK_CNPJ_SUPPLIER UNIQUE KEY (cnpj),
+    CONSTRAINT FRK_ID_SUPPLIER FOREIGN KEY (id_people) REFERENCES people(id_people)
 );
 
-CREATE TABLE IF NOT EXISTS funcionario(
-	cargo varchar(255) not null,
-    id_pessoa int not null,
-    CONSTRAINT FRK_ID_FUNCIONARIO FOREIGN KEY (id_pessoa) REFERENCES pessoa(id_pessoa)
+CREATE TABLE IF NOT EXISTS employee(
+	role varchar(255) not null,
+    id_people int not null,
+    CONSTRAINT FRK_ID_EMPLOYEE FOREIGN KEY (id_people) REFERENCES people(id_people)
 );
 
-CREATE TABLE IF NOT EXISTS usuario(
+CREATE TABLE IF NOT EXISTS user(
 	login varchar(255) not null,
-    senha varchar(255) not null,
-    id_funcionario int not null,
-    CONSTRAINT UNK_LOGIN_USUARIO UNIQUE KEY (login),
-    CONSTRAINT FRK_ID_USUARIO FOREIGN KEY (id_funcionario) REFERENCES funcionario(id_pessoa)
+    password varchar(255) not null,
+    id_employee int not null,
+    CONSTRAINT UNK_LOGIN_USER UNIQUE KEY (login),
+    CONSTRAINT FRK_ID_USER FOREIGN KEY (id_employee) REFERENCES employee(id_people)
 );
 
-INSERT INTO pessoa (nome_pessoa, telefone_pessoa) VALUES ('Administrador', '199XXXXYYYY');
-INSERT INTO funcionario VALUES('Gerente', 1);
-INSERT INTO usuario VALUES ('admin', MD5('admin'), 1);
+INSERT INTO people (name_people, phone_people) VALUES ('Administrador', '199XXXXYYYY');
+INSERT INTO employee VALUES('Gerente', 1);
+INSERT INTO user VALUES ('admin', MD5('admin'), 1);
