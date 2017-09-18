0d793059bd8d6c9c29fe6ac8b4ef7b549268b925
felipemantoan <luisfelipemantoan@gmail.com>
2017-09-17 22:03:04 -0300

    Novo ponto de entrada para a aplicação

--- src/controller/controller/Controller.java
+++ src/controller/controller/Controller.java
@@ -5,26 +5,48 @@
  */
 package controller.controller;
 
+import java.io.IOException;
 import javafx.application.Application;
+import static javafx.application.Application.launch;
 import javafx.application.Platform;
-import javafx.fxml.Initializable;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 
 /**
  *
  * @author SAMSUNG
  */
-public abstract class Controller extends Application implements Initializable{
+public class Controller extends Application {
     
-    protected Stage window;
-    
-    public abstract void controllerUndecored(Parent root, Stage stage);
-    
-    public abstract void controllerDecored(Parent root, Stage stage);
-    
+    public Stage createStageInstance(Stage stage, String path) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource(path));
+        stage.setScene(new Scene(root));
+        return stage;
+    }
+   
     public void closeApplication() {
         Platform.exit();
     }
+    
+    
+    @Override
+    public void start(Stage PrimaryStage) {
+        try {
+            this.createStageInstance(PrimaryStage, "controller/login/Login.fxml");
+            PrimaryStage.show();
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(Controller.class, args);
+    }
+
 }
