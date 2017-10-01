5519db774803e028b1ac6cb6418a2cd86f967c5a
MatheusHNSantos <matheushenriki10@hotmail.com>
2017-10-01 18:49:16 -0300

    Criação de classes DAO
    Criação das classes: AddressDAO, CostumerDAO, EmployeeDAO, IngredientDAO, ItemsSaleDAO, PhoneDAO, ProductDAO, ProductIngredientDAO, ProductTypeDAO, SaleDAO, SupplierDAO.

---
+++ src/dao/AddressDAO.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import java.util.ArrayList;
+import model.ModelInterface;
+
+/**
+ *
+ * @author Matheus Henrique
+ */
+public class AddressDAO implements CRUDInterface{
+
+    @Override
+    public boolean create(ModelInterface model) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean read(ModelInterface model) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean update(ModelInterface model) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean delete(ModelInterface model) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ArrayList readAll(ModelInterface model) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+
+}
