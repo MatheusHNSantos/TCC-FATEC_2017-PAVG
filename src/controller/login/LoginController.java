1ce0106f041a3ada9eb3f396dfad1b75f22fdf9c
felipemantoan <luisfelipemantoan@gmail.com>
2017-10-08 16:46:08 -0300

    Removidas as bibliotecas

--- src/controller/login/LoginController.java
+++ src/controller/login/LoginController.java
@@ -8,8 +8,6 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import controller.controller.Controller;
