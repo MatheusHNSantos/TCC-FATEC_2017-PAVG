e3b31c153047f2a97b474445386d4cdc0c61dd08
felipemantoan <luisfelipemantoan@gmail.com>
2017-10-28 08:13:29 -0200

    Remoção de métodos

--- src/main/java/dao/entity/DAO.java
+++ src/main/java/dao/entity/DAO.java
@@ -3,11 +3,4 @@ package dao.entity;
 public interface DAO {
 
     public static int LAST_ID_INSERT = -1;
-
-    public boolean create();
-    public boolean update();
-    public void load();
-    public boolean delete();
-
-    public void createInstance();
 }
