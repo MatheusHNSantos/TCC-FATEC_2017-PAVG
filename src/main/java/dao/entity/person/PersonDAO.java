78e9e0942c530c11037f2ae371ebb161b88eceac
felipemantoan <luisfelipemantoan@gmail.com>
2017-11-01 20:16:54 -0200

    Mudança nas excessões

--- src/main/java/dao/entity/person/PersonDAO.java
+++ src/main/java/dao/entity/person/PersonDAO.java
@@ -13,6 +13,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 /**
@@ -23,43 +25,53 @@ abstract class PersonDAO implements DAO{
 
     public static int LAST_ID_INSERT = -1;
 
-    public static boolean create(Person person) throws SQLException, ClassNotFoundException {
+    public static boolean create(Person person) {
+
         Connection conn = ConnectionFactory.getConnection();
         String sql = "INSERT INTO person (id_address, name_person) VALUES (?, ?)";
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
 
-        PreparedStatement stmt = conn.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS);
-
-        stmt.setInt(1, person.getAddress().getId());
-        stmt.setString(2, person.getName());
-
-        int affectedRows = stmt.executeUpdate();
-
-        if (affectedRows > 0) {
-            ResultSet rs = stmt.getGeneratedKeys();
+        try {
+            stmt = conn.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS);
+            stmt.setInt(1, person.getAddress().getId());
+            stmt.setString(2, person.getName());
+            stmt.execute();
+            stmt.getGeneratedKeys();
             rs.next();
             LAST_ID_INSERT = rs.getInt(1);
-            ConnectionFactory.closeConnection(conn, stmt, rs);
             return true;
         }
+        catch (SQLException sqlE) {
+            Logger.getLogger(PersonDAO.class.getName()).log( Level.SEVERE, null, sqlE);
+        }
+        finally {
+            ConnectionFactory.closeConnection(conn, stmt, rs);
+        }
 
-        ConnectionFactory.closeConnection(conn, stmt);
         return false;
     }
 
-    public static boolean update(Person person) throws SQLException, ClassNotFoundException {
+    public static boolean update(Person person) {
+
         Connection conn = ConnectionFactory.getConnection();
         String sql = "UPDATE person SET id_address = ?, name_person = ? WHERE id_person = ?";
-        PreparedStatement stmt = conn.prepareStatement(sql);
+        PreparedStatement stmt = null;
 
-        stmt.setInt(1, person.getAddress().getId());
-        stmt.setString(2, person.getName());
-        stmt.setInt(3, person.getId());
+        try {
+            stmt = conn.prepareStatement(sql);
+            stmt.setInt(1, person.getAddress().getId());
+            stmt.setString(2, person.getName());
+            stmt.setInt(3, person.getId());
 
-        int affectedRows = stmt.executeUpdate();
-        ConnectionFactory.closeConnection(conn, stmt);
-
-        if (affectedRows > 0) {
+            stmt.execute();
             return true;
+        }
+        catch (SQLException sqlE) {
+            Logger.getLogger(PersonDAO.class.getName()).log( Level.SEVERE, null, sqlE);
+        }
+        finally {
+            ConnectionFactory.closeConnection(conn, stmt);
         }
 
         return false;
